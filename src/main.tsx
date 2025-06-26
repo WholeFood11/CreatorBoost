610027a8e1824aa5aee7f597fb191c7a83c88317
WholeFood11 <127201427+WholeFood11@users.noreply.github.com>
2025-06-26 16:23:43 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   console.log("HI");
-  // <StrictMode>
-  //   <App />
-  // </StrictMode>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
